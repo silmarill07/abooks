677e9eb30a89d12bdbe5f20e4bc923bc472e63bb
silmarill07 <silmarill07@gmail.com>
2025-09-11 15:36:43 +0300

    remove ishod

--- books.js
+++ books.js
@@ -1,37 +1,5 @@
 const books = [
     {
-        id: "ishod",
-        title: "Андрей Круз, Мария Круз - Земля лишних.Исход",
-        cover: "img/исход.jpg",
-        audioFiles: [
-        { title: "Файл 1", file: "audio/Isxod/01.mp3" },
-        { title: "Файл 2", file: "audio/Isxod/02.mp3" },
-        { title: "Файл 3", file: "audio/Isxod/03.mp3" },
-        { title: "Файл 4", file: "audio/Isxod/04.mp3" },
-        { title: "Файл 5", file: "audio/Isxod/05.mp3" },
-        { title: "Файл 6", file: "audio/Isxod/06.mp3" },
-        { title: "Файл 7", file: "audio/Isxod/07.mp3" },
-        { title: "Файл 8", file: "audio/Isxod/08.mp3" },
-        { title: "Файл 9", file: "audio/Isxod/09.mp3" },
-        { title: "Файл 10", file: "audio/Isxod/10.mp3" },
-        { title: "Файл 11", file: "audio/Isxod/11.mp3" },
-        { title: "Файл 12", file: "audio/Isxod/12.mp3" },
-        { title: "Файл 13", file: "audio/Isxod/13.mp3" },
-        { title: "Файл 14", file: "audio/Isxod/14.mp3" },
-        { title: "Файл 15", file: "audio/Isxod/15.mp3" },
-        { title: "Файл 16", file: "audio/Isxod/16.mp3" },
-        { title: "Файл 17", file: "audio/Isxod/17.mp3" },
-        { title: "Файл 18", file: "audio/Isxod/18.mp3" },
-        { title: "Файл 19", file: "audio/Isxod/19.mp3" },
-        { title: "Файл 20", file: "audio/Isxod/20.mp3" },
-        { title: "Файл 21", file: "audio/Isxod/21.mp3" },
-        { title: "Файл 22", file: "audio/Isxod/22.mp3" },
-        { title: "Файл 23", file: "audio/Isxod/23.mp3" },
-        { title: "Файл 24", file: "audio/Isxod/24.mp3" },
-        { title: "Файл 25", file: "audio/Isxod/25.mp3" }
-        ]
-    },
-    {
     id: "novaja",
     title: "Андрей Круз, Мария Круз - Земля лишних.Новая жизнь",
     cover: "img/новая жизнь.jpg",
